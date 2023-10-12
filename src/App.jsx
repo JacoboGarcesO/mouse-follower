0a871ea97ee2ff87e102ef56d3bfa35379f0f03c
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2023-10-12 07:14:37 -0500

    Push finished project

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  // El useEffect se ejecuta cuando se monta el componente
+  // por primera vez y se vuelve a ejecutar cuando cambian
+  // sus dependencias
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  useEffect(() => {
+    if (enabled) {
+      document.body.classList.add('no-cursor')
+    }
+
+    return () =>{
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }} />
+
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguimiento del puntero
+      </button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
